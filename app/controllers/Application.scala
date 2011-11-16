911ade7f8f4b6d7c37198ad26b477382cfdba4cd
Gaetan Renaudeau <renaudeau.gaetan@gmail.com>
2011-11-16 16:26:30 +0100

    Bootstrap index action

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,10 +5,27 @@ import play.api.mvc._
 
 import models._
 
+import play.api.libs.concurrent._
+
 object Application extends Controller {
   
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    val links = HackerNewsRetriever.getLinks()
+    AsyncResult(
+      links.map(links => {
+        Logger.debug(links.length+" links retrieved.");
+        val images = links.flatMap(link => {
+          val imageUrl = MostRelevantPageImageExtractor.getImageUrl(link.url)
+          imageUrl.value /* FIXME this is blocking... */ match {
+            case Redeemed(url) => url.map(
+              url=>link.copy(url=url)
+            )
+          }
+        })
+        Logger.debug(images.length+" images retrieved.");
+        Ok(views.html.index(images))
+      })
+    )
   }
 
   def testHackerNews = Action {
