926f77402917de82347bd4566622803316d2e412
Gaetan Renaudeau <renaudeau.gaetan@gmail.com>
2011-12-12 11:09:08 +0100

    Add async sequence promise ( List[Promise] => Promise[List] )

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -30,8 +30,10 @@ object Application extends Controller {
   }
 
   def get(format:String) = Action { (request) =>
-    val linksRetriever:LinksRetriever = request.queryString.get("service").flatMap(_.headOption).flatMap(linksRetrieverModes.get(_)).getOrElse(HackerNewsRetriever)
-    val imageExtractor:ImageExtractor = request.queryString.get("image"  ).flatMap(_.headOption).flatMap(imageExtractorModes.get(_)).getOrElse(ScreenshotExtractor)
+    val linksRetriever:LinksRetriever = request.queryString.get("service").flatMap(_.headOption).
+      flatMap(linksRetrieverModes.get(_)).getOrElse(HackerNewsRetriever)
+    val imageExtractor:ImageExtractor = request.queryString.get("image"  ).flatMap(_.headOption).
+      flatMap(imageExtractorModes.get(_)).getOrElse(ScreenshotExtractor)
     AsyncResult(
       getResult(linksRetriever, imageExtractor).extend(promise => {
         promise.value match {
@@ -45,8 +47,7 @@ object Application extends Controller {
   }
 
   implicit def linkWithImageWrites : Writes[LinkWithImage] = new Writes[LinkWithImage] {
-    def writes(o: LinkWithImage):JsObject = {
-      JsObject(Map(
+    def writes(o: LinkWithImage) = JsObject(Map(
       "url" -> JsString(o.link.url),
       "weight" -> JsNumber(o.link.weight),
       "title" -> JsString(o.link.title),
@@ -54,22 +55,21 @@ object Application extends Controller {
       "feedbackText" -> JsString(o.link.feedbackText),
       "image" -> JsString(o.image.url)
     ))
-    }
+  }
+
+  def sequencePromises[A](list: List[Promise[A]]): Promise[List[A]] = {
+    list.foldLeft(Promise.pure(List[A]()))((s,p) => s.flatMap( s => p.map(a => s :+ a))) 
   }
 
   def getResult(linksRetriever:LinksRetriever, imageExtractor:ImageExtractor):Promise[List[LinkWithImage]] = {
     val links = linksRetriever.getLinks()
-    links.map(links => {
+    links.flatMap(links => {
       Logger.debug(links.length+" links found.");
-      val images = links.flatMap(link => {
-        val image = imageExtractor.getImage(link.url)
-        image.value match { // FIXME this is blocking... 
-          case Redeemed(url) => url.map(
-            img => LinkWithImage(link, img)
-          )
-        }
-      })
-      Logger.debug(images.length+" images found.");
+      val images = sequencePromises(links.map(link => imageExtractor.getImage(link.url).map( (link, _) )) ).map(_.flatMap(_ match {
+        case (link, Some(img)) => Some(LinkWithImage(link, img))
+        case _ => None
+      }))
+      images.map(images => Logger.debug(images.length+" images found."));
       images
     })
   }
